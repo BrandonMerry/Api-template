f03feb74abbdf75c0770b72a9977b03ba61a356d
brandon <brandon.l.merry@gmail.com>
2023-04-03 23:48:55 +0700

    created user model schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Scheema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add a an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+  },
+})
