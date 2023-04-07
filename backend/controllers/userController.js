1cf6df20cd526cdf1285a7e305e352d233b67746
brandon <brandon.l.merry@gmail.com>
2023-04-07 23:13:49 +0700

    updated readme file

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,39 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// asyncHandler(async .. makes everything async
+
 // description Register a  User
 // route POST /api/users
 // access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  // destured body data
+  const { name, email, password } = req.body
+
+  // validation
+  if (!name || email || password) {
+    res.status(400)
+    throw new Error('please add all the fields')
+  }
+
   res.json({ message: 'Register a user here' })
-}
+})
 
 // description User Regestration
 // route POST /api/login
 // access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: 'user login' })
-}
+})
 
 // description gets users data form DB
 // route GET /api/users/me
 // access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'returns user data' })
-}
+})
 
 module.exports = {
   registerUser,
---
+++ readMe.md
@@ -0,0 +1,8 @@
+Objectives
+
+- use postman
+- jwt tokens / auth
+- crud in react
+- spinning up create-react-app
+- Get / post /put / delete in React
+- Learn broad concepts to input into anki for further studying
